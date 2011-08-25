615ccbc41d2540eaeb3742b5d9577343fc16f1f5
Anum <anum.qudsia@dai.co.uk>
2011-08-25 15:21:56 +0100

    .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App1::Application.config.secret_token = 'c9d174bf2210621935a3a5ffb15007bc76aa6fdc7f94e7e261c95575ccfcf3e0d68cd5c52dcebf8cb451cea94a3e4b8c9e41fa837b968386bb6a0dcc8ce6c91c'
